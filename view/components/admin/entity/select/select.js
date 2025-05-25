9383124fb00dbff28ebac15b2a917a08902ec24b
arikaim <info@arikaim.com>
2025-05-25 13:27:34 +0300

    =

--- view/components/admin/entity/select/select.js
+++ view/components/admin/entity/select/select.js
@@ -4,14 +4,29 @@ arikaim.component.onLoaded(function() {
     arikaim.ui.button('.new-entity-button',function(element) {
         var contentId = $(element).attr('content-id');
         var role = $(element).attr('role');
-        $('#' + contentId).show();
-
+      
         arikaim.ui.loadComponent({
             mountTo: contentId,
             params: {
                 role: role,
             },           
-            name: 'entity::admin.entity.panel'
+            name: 'entity::admin.entity.add'
         })
+    });
+
+    arikaim.ui.button('.edit-entity-button',function(element) {
+        var contentId = $(element).attr('content-id');
+        var dropdownId = $(element).attr('dropdown-id');
+        var uuid = arikaim.ui.getComponent(dropdownId).get('selected');
+
+        if (isEmpty(uuid) == false) {
+            arikaim.ui.loadComponent({
+                mountTo: contentId,
+                params: {
+                    uuid: uuid
+                },           
+                name: 'entity::admin.entity.edit'
+            });
+        }
     });
 });

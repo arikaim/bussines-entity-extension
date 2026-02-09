4efa6126bca838aa66dd5b0bdb057814f816c114
arikaim <info@arikaim.com>
2026-02-09 20:27:49 +0200

    =

--- view/components/admin/entity/select/select.js
+++ view/components/admin/entity/select/select.js
@@ -1,32 +1,39 @@
 'use strict';
 
-arikaim.component.onLoaded(function() {  
-    arikaim.ui.button('.new-entity-button',function(element) {
-        var contentId = $(element).attr('content-id');
-        var role = $(element).attr('role');
-      
-        arikaim.ui.loadComponent({
-            mountTo: contentId,
-            params: {
-                role: role,
-            },           
-            name: 'entity::admin.entity.add'
-        })
-    });
-
-    arikaim.ui.button('.edit-entity-button',function(element) {
-        var contentId = $(element).attr('content-id');
-        var dropdownId = $(element).attr('dropdown-id');
-        var uuid = arikaim.ui.getComponent(dropdownId).get('selected');
-
-        if (isEmpty(uuid) == false) {
+arikaim.component.onLoaded(function(component) { 
+    
+    component.init = function() {
+        arikaim.ui.button('.new-entity-button',function(element) {
             arikaim.ui.loadComponent({
-                mountTo: contentId,
+                mountTo: component.get('content-id'),
                 params: {
-                    uuid: uuid
+                    role: component.get('role'),
                 },           
-                name: 'entity::admin.entity.edit'
-            });
-        }
-    });
+                name: 'entity::admin.entity.add'
+            })
+        });
+
+        arikaim.ui.button('.edit-entity-button',function(element) {
+            var select = arikaim.ui.getComponent(component.get('dropdown-id'))      
+            var uuid = select.getValue();
+
+            arikaim.events.on('entity.update',function() {
+                select.reinit();
+            },'entityUpdateSelect');
+
+                if (isEmpty(uuid) == false) {
+                    arikaim.ui.loadComponent({
+                        mountTo: component.get('content-id'),
+                        params: {
+                            uuid: uuid
+                        },           
+                        name: 'entity::admin.entity.edit'
+                    });
+                }
+        });
+    }
+    
+    component.init();
+
+    return component;
 });
